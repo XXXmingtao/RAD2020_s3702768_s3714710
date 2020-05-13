4fef6f150db82eadea7a9af926e4ea5315a70f4d
Mingtao Liu <s3714710@student.rmit.edu.au>
2020-05-14 02:33:17 +1000

    finish signup_part work

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   #root 'session#new'
   get '/login', to: 'sessions#new'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
