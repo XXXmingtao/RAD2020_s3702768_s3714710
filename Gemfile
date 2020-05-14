e4f42889112221b23ec0a63a75697e1eebafb1c6
Mingtao Liu <s3714710@student.rmit.edu.au>
2020-05-15 00:20:38 +1000

    finish login part

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.7'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt', '3.1.12'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+gem 'rails-ujs', '~> 0.1.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
-    def new
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+       log_in user
+       redirect_to user
+    else
+    flash.now[:danger] = "Invalid Credential"
+    render 'new'
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to signup_url
+  end
 end
