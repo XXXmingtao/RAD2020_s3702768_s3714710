460f61eadde6821c07f1a06ec4eac689a3573a9d
Tianwei <s3702768@student.rmit.edu.au>
2020-05-16 23:52:17 +1000

    The Recent Members can now display users who have posted recently. The topic News will no longer displayed in Topic area.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,10 @@ class StaticPagesController < ApplicationController
     @posts = Post.order('created_at DESC')
     @users = User.all
     @toolbar_posts = Post.order('view DESC').limit(5)
+    @active_users = Array.new
+    @posts.each do |post|
+      @active_users.push(User.find(post.author_id)) unless @active_users.include?(User.find(post.author_id))
+      end
   end
 
   def help
