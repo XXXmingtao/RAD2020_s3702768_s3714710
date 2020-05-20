58639075906eca95ae3d7d8fb553c972f64b52b6
Tianwei <s3702768@student.rmit.edu.au>
2020-05-21 02:53:19 +1000

    Fix the bug in userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,12 @@ class UsersController < ApplicationController
 
   def show
     @user=User.find_by_id(params[:id])
-    @posts = Post.where(user_id:@user.id)
-    @comments = Comment.where(user_id:@user.id)
-    render 'user_not_found' if @user.nil?
+    if @user.nil?
+      render 'user_not_found' 
+    else
+      @posts = Post.where(user_id:@user.id)
+      @comments = Comment.where(user_id:@user.id)
+    end
   end
     
   def new
