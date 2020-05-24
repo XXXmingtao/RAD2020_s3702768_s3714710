26bfaac22cdccf6ccca30ff520fbe8854b214514
Mingtao Liu <s3714710@student.rmit.edu.au>
2020-05-24 14:11:28 +1000

    password reset

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,6 +20,10 @@ class UserTest<ActiveSupport::TestCase
         assert_not @user.valid?
     end
 
+    test "authenticated? should return false for a user with nil digest" do
+        assert_not @user.authenticated?(:remember, '')
+    end
+
 
     test "name should be present" do
         @user.name="     "
@@ -79,9 +83,5 @@ class UserTest<ActiveSupport::TestCase
     test "password should have a minimum length" do
         @user.password=@user.password_confirmation="a" * 5
         assert_not@user.valid?
-    end
-
-    test "authenticated? should return false for a user with nil digest" do
-        assert_not @user.authenticated?('')
     end
 end
