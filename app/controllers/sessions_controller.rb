0e49f4f271171ec76bbb0670c78bcad3a99abade
Tianwei <s3702768@student.rmit.edu.au>
2020-05-24 22:33:50 +1000

    User can now login with their username and the username should be unique while signing up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user=User.find_by(email: params[:session][:email].downcase)
+    user=User.find_by(name: params[:session][:username])
+    if user.nil?
+      user=User.find_by(email: params[:session][:username].downcase)
+    end
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
-       log_in user
-        params[:session][:remember_me]=='1'? remember(user) : forget(user)
-        redirect_back_or user
-      else
-        message="Account not activated."
-        message+="Check your email for the activation link."
-        flash[:warning]=message
-        redirect_to root_url
-      end
+      log_in user
+      params[:session][:remember_me]=='1'? remember(user) : forget(user)
+      redirect_back_or user
     else
-      flash.now[:danger]= 'Invalid email/password combination'
+      flash.now[:danger]= 'Invalid username/password combination'
       render 'new'
     end
   end
